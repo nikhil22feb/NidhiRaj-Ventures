83ec148a57e6376bdcc861635edae3be618ecf0f
nikhil22feb <nikhilrajverma80@gmail.com>
2026-04-28 17:37:35 +0530

    fix migrate

--- ecommerce/wsgi.py
+++ ecommerce/wsgi.py
@@ -1,6 +1,14 @@
-
 import os
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ecommerce.settings')
+
+# 🔥 AUTO MIGRATE (IMPORTANT)
+from django.core.management import call_command
+try:
+    call_command('migrate', interactive=False)
+    call_command('collectstatic', interactive=False)
+except Exception as e:
+    print("Migration error:", e)
+
 application = get_wsgi_application()
