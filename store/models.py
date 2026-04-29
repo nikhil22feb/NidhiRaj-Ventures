b351fa31fa959ce9f789031298460a4e1c5a486e
nikhil22feb <nikhilrajverma80@gmail.com>
2026-04-29 13:44:55 +0530

    switch to cloudinary image field

--- store/models.py
+++ store/models.py
@@ -1,5 +1,6 @@
 
 from django.db import models
+from cloudinary.models import CloudinaryField
 
 class Category(models.Model):
     name = models.CharField(max_length=100)
@@ -12,7 +13,8 @@ class Product(models.Model):
     category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='products')
     price = models.IntegerField()
     description = models.TextField(blank=True)
-    image = models.ImageField(upload_to='products/', blank=True, null=True)
+    # image = models.ImageField(upload_to='products/', blank=True, null=True)
+    image = CloudinaryField('image')
 
     def __str__(self):
         return self.name
